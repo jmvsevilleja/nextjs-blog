06ae3ed097842053b642f2b970accf702b0ebdf9
Jess Mark Sevilleja <jess@clinica.dev>
2023-01-24 13:22:26 +0800

    added remark date link and styles

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from '../styles/alert.module.css';
 import Link from "next/link";
 import clsx from 'clsx';
 import {getSortedPostsData} from '../lib/posts';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -54,11 +55,11 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
